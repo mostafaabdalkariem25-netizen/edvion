4dde8df0502844689312972e21422ce1a6731f4a
mostafaabdalkariem25-netizen <mostafaabdalkariem25@gmail.com>
2025-09-15 21:10:38 +0300

    Add files via upload

---
+++ lip.js
@@ -0,0 +1,11 @@
+// lib.js
+
+// دالة اختيار عنصر واحد
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+// دالة اختيار كل العناصر المطابقة
+function $all(selector) {
+  return document.querySelectorAll(selector);
+}
